c16e881562a80562e234bf5e1aa04f3d62c9035d
D <phuuuumi@gmail.com>
2026-01-09 15:22:29 +0700

    lab14_1 completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -2,9 +2,7 @@
 using namespace std;
 
 template <typename T>
-void insertionSort(T d[],int N){
-
-}
+void insertionSort(T d[],int N);
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
@@ -16,4 +14,19 @@ int main(){
 	
 	cout << "\nSorted Array:";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
+}
+
+template <typename T>
+void insertionSort(T d[],int N){
+	for(int i=1 ; i<N; i++){
+		T x = d[i];
+		for(int j=i-1 ; j>=0; j--){
+			if(x > d[j]) swap(d[j], d[j+1]);
+			else break;
+		}
+
+		cout << "Pass " << i << ":";
+		for(int j=0; j<N; j++) cout << d[j] << " ";
+		cout << "\n";
+	}
 }
